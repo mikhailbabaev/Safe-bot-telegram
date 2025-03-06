1c187f72c3d0cb4fd71fcb4bb6f81b70a3491c24
Mikhail <mikhail.a.babaev@gmail.com>
2025-03-06 20:36:25 +0300

    Add reference-system and some fixes

---
+++ bot/keyboards/common_keyboards.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from templates.buttons import FAQ, PAYMENT, REFERENCE, CHECK_SECURITY, ACHIVEMENTS
+
+start_menu = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text=CHECK_SECURITY, callback_data="check_security")],
+    [InlineKeyboardButton(text=PAYMENT, callback_data="payments")],
+    [InlineKeyboardButton(text=REFERENCE, callback_data="cashback")],
+    [InlineKeyboardButton(text=ACHIVEMENTS, callback_data='achivements')],
+    [InlineKeyboardButton(text=FAQ, callback_data="FAQ")]
+])
+
+
